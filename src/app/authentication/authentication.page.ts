80b9467a6aa4ee24298db89811e4de1f724de0c8
Demircan Erol <edemircan@adista.fr>
2022-02-19 10:08:35 +0100

    feat(auth): add login form

--- src/app/authentication/authentication.page.ts
+++ src/app/authentication/authentication.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-authentication',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authentication.page.scss'],
 })
 export class AuthenticationPage implements OnInit {
-
-  constructor() { }
+  credentials: FormGroup
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthenticationService,
+    private alertController: AlertController,
+    private router: Router,
+    private loadingController: LoadingController
+  ) { }
 
   ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['email@email.com', [Validators.required, Validators.email]],
+      password: ['password', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  async login() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    
+    this.authService.login(this.credentials.value).subscribe(
+      async (res) => {
+        await loading.dismiss();        
+        this.router.navigateByUrl('/tabs', { replaceUrl: true });
+      },
+      async (res) => {
+        await loading.dismiss();
+        const alert = await this.alertController.create({
+          header: 'Login failed',
+          message: res.error.error,
+          buttons: ['OK'],
+        });
+ 
+        await alert.present();
+      }
+    );
+  }
+  get email() {
+    return this.credentials.get('email');
+  }
+  get password() {
+    return this.credentials.get('password');
+  }
 }
